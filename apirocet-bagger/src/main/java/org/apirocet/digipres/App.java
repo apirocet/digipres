ff139fb84bd984eb808148e74582f42068003182
Scott Prater <prater@wisc.edu>
2019-12-20 15:41:11 -0600

    Bagger utility using BagIt spec 1.0

---
+++ apirocet-bagger/src/main/java/org/apirocet/digipres/App.java
@@ -0,0 +1,13 @@
+package org.apirocet.digipres;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
