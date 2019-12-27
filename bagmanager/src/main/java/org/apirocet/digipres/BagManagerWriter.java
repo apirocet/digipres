73434f699ceb4c6986483d5738bf04b042fa961a
Scott Prater <prater@wisc.edu>
2019-12-27 15:23:46 -0600

    Write bag to different folder

--- bagmanager/src/main/java/org/apirocet/digipres/BagManagerWriter.java
+++ bagmanager/src/main/java/org/apirocet/digipres/BagManagerWriter.java
@@ -3,16 +3,16 @@ package org.apirocet.digipres;
 import gov.loc.repository.bagit.creator.BagCreator;
 import gov.loc.repository.bagit.domain.Bag;
 import gov.loc.repository.bagit.hash.StandardSupportedAlgorithms;
+import gov.loc.repository.bagit.reader.BagReader;
 import gov.loc.repository.bagit.verify.BagVerifier;
 import org.slf4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -24,6 +24,7 @@ public class BagManagerWriter {
     private File outbagdir;
     private Bag bag;
     private boolean includeHiddenFiles = false;
+    private boolean replace = false;
     private StandardSupportedAlgorithms algorithm;
 
     public BagManagerWriter(File bagdir, StandardSupportedAlgorithms algorithm) {
@@ -31,71 +32,141 @@ public class BagManagerWriter {
         this.algorithm = algorithm;
     }
 
+    public BagManagerWriter(File bagdir, File outbagdir, StandardSupportedAlgorithms algorithm, boolean replace) {
+        this.bagdir = bagdir;
+        this.outbagdir = outbagdir;
+        this.algorithm = algorithm;
+        this.replace = replace;
+    }
+
     public Integer write() {
         if (outbagdir == null) {
-            return writeInPlace() && verify() ? 0 : 1;
+            return writeInPlace(Paths.get(bagdir.getAbsolutePath())) && verify(bagdir) ? 0 : 1;
         } else {
-            return writeElsewhere();
+            return writeElsewhere(Paths.get(bagdir.getAbsolutePath()), Paths.get(outbagdir.getAbsolutePath())) && verify(outbagdir) ? 0 : 1;
         }
     }
 
-    private Integer writeElsewhere() {
-        return 0;
-    }
-
-    private Boolean writeInPlace() {
-        Path folder = Paths.get(bagdir.getAbsolutePath());
-
+    private boolean writeInPlace(Path folder) {
         if (Files.notExists(folder)) {
-            System.out.println("Cannot create in-place bag at '" + bagdir +"': directory does not exist");
-            LOGGER.error("Cannot create in-place bag at '{}': directory does not exist", bagdir);
+            LOGGER.error("Cannot create in-place bag at '{}': directory does not exist", folder);
             return false;
         }
 
         if (isBag(folder)) {
-            System.out.println("Cannot create in-place bag at '" + bagdir +"': bag already exists at this location");
-            LOGGER.error("Cannot create in-place bag at '{}': bag already exists at this location", bagdir);
+            LOGGER.error("Cannot create in-place bag at '{}': bag already exists at this location", folder);
             return false;
         }
 
-        System.out.println("Creating bag in place from contents at '" + bagdir.getAbsolutePath() + "' with " + algorithm.getMessageDigestName() + " checksums");
-        LOGGER.info("Creating bag in place from contents at '{}' with '{}' checksums", bagdir.getAbsolutePath(), algorithm.getMessageDigestName());
+        LOGGER.info("Creating bag in place from contents at '{}' with {} checksums", folder.toAbsolutePath(), algorithm.getMessageDigestName());
 
         try {
-            bag = BagCreator.bagInPlace(folder, Arrays.asList(algorithm), includeHiddenFiles);
+            bag = BagCreator.bagInPlace(folder, Collections.singletonList(algorithm), includeHiddenFiles);
         } catch (NoSuchAlgorithmException ex) {
-            System.err.println("Cannot create bag with checksum algorithm '" + algorithm.getMessageDigestName() +"': " + ex.getMessage());
-            LOGGER.error("Cannot create bag with checksum algorithm '{}': {}", algorithm.getMessageDigestName(), ex.getMessage());
+            LOGGER.error("Cannot create bag with checksum algorithm {}: {}", algorithm.getMessageDigestName(), ex.getMessage());
             return false;
         } catch (IOException ex) {
-            System.err.println("Cannot create in-place bag at '" + folder +"': " + ex.getMessage());
             LOGGER.error("Cannot create in-place bag at '{}': {}", folder, ex.getMessage());
             return false;
         }
 
-        System.out.println("Bag created at '" + bagdir.getAbsolutePath() + "'");
-        LOGGER.info("Bag created at '{}'", bagdir.getAbsolutePath());
-        
+        LOGGER.info("Bag created at '{}'", folder.toAbsolutePath());
+
         return true;
     }
 
-    private Boolean verify() {
+    private boolean writeElsewhere(Path srcfolder, Path outfolder) {
+        boolean removeExistingBag = false;
+
+        Path absSrc = srcfolder.toAbsolutePath().normalize();
+        Path absDest = outfolder.toAbsolutePath().normalize();
+        if (absSrc.toString().equals(absDest.toString())) {
+            LOGGER.error("Cannot create bag:  source and destination are the same", outfolder);
+            return false;
+        }
+
+        if (Files.exists(outfolder)) {
+            if (isBag(outfolder)) {
+                if (!replace) {
+                    LOGGER.error("Cannot create bag at '{}': bag already exists at this location.  Please use the '--replace' flag to replace the contents of this bag", outfolder);
+                    return false;
+                } else {
+                    removeExistingBag = true;
+                }
+            } else {
+                LOGGER.error("Cannot create bag at '{}': directory exists", outfolder);
+                return false;
+            }
+        }
+
+        if (removeExistingBag) {
+            LOGGER.info("Removing existing bag at '{}'", outfolder);
+            Path delPath = outfolder.resolve(outfolder);
+
+            try {
+                Files.walk(delPath).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+            } catch (IOException ex) {
+                LOGGER.error("Cannot clean bag destination", ex);
+                return false;
+            }
+        }
+
+        try {
+            copyFolder(srcfolder.toFile(), outfolder.toFile());
+        } catch (IOException ex) {
+            LOGGER.error("Cannot copy source contents to output bag folder", ex);
+            return false;
+        }
+
+        return writeInPlace(outfolder);
+    }
+
+    private boolean verify(File bagdir) {
         BagVerifier bv = new BagVerifier();
+        BagReader reader = new BagReader();
         LOGGER.info("Verifying valid bag from contents at '{}'", bagdir.getAbsolutePath());
         try {
+            Bag bag = reader.read(bagdir.toPath());
             bv.isValid(bag, includeHiddenFiles);
+        } catch (NoSuchFileException ex) {
+            LOGGER.error("'{}' does not appear to be a bag:  missing bagit.txt file", bagdir);
+            return false;
         } catch (Exception ex) {
-            LOGGER.error(ex.getMessage());
+            LOGGER.error("Bag is not valid", ex);
             return false;
         }
 
         return true;
     }
 
-    private Boolean isBag(Path folder) {
+    private boolean isBag(Path folder) {
         String bagitfile = folder.toString() + File.separatorChar + "bagit.txt";
         Path bag = folder.resolve(bagitfile);
-        System.out.println(bag);
         return Files.exists(bag);
     }
+
+    private void copyFolder(File srcfolder, File destfolder) throws IOException
+    {
+        if (srcfolder.isDirectory())
+        {
+            if (!destfolder.exists())
+            {
+                destfolder.mkdir();
+            }
+
+            String files[] = srcfolder.list();
+            for (String file : files)
+            {
+                File srcFile = new File(srcfolder, file);
+                File destFile = new File(destfolder, file);
+
+                copyFolder(srcFile, destFile);
+            }
+        }
+        else
+        {
+            Files.copy(srcfolder.toPath(), destfolder.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
 }
+
--- bagmanager/src/main/java/org/apirocet/digipres/CmdBagManagerWriter.java
+++ bagmanager/src/main/java/org/apirocet/digipres/CmdBagManagerWriter.java
@@ -14,19 +14,42 @@ public class CmdBagManagerWriter implements Callable<Integer> {
     @CommandLine.ParentCommand
     private BagManager bm;
 
-    @CommandLine.Parameters(paramLabel = "<bag directory>", description = "the path to the bag directory")
+    @CommandLine.Parameters(paramLabel = "<source directory>", description = "the path to the source directory")
     File bagdir;
 
-    @CommandLine.Option(names = {"-a", "--algorithm"}, defaultValue = "MD5", description = "checksum algorithm to use. Supported algorithms are MD5, SHA1, SHA224, SHA256, and SHA512.  Default is MD5.")
+    @CommandLine.Option(names = {"-a", "--algorithm"},
+            defaultValue = "MD5",
+            description = "checksum algorithm to use. Supported algorithms are MD5, SHA1, SHA224, SHA256, and SHA512.  Default is MD5.")
     StandardSupportedAlgorithms algorithm;
 
+    @CommandLine.ArgGroup(exclusive = false)
+    ElsewhereOptions eogroup;
+    static class ElsewhereOptions {
+        @CommandLine.Option(names = {"-o", "--outdir"},
+                required = true,
+                description = "the path to the output directory where the bag will be created.  Optional:  if not specified, the bag will be created in-place at the source directory")
+        File outbagdir;
+
+        @CommandLine.Option(names = {"-r", "--replace"},
+                required = false,
+                description = "replace the bag contents in the output bag directory")
+        boolean replace;
+    }
 
     @Override
     public Integer call() throws IOException, NoSuchAlgorithmException {
         if (bm.logfile != null) {
             System.setProperty("logfile", bm.logfile);
         }
-        BagManagerWriter bw = new BagManagerWriter(bagdir, algorithm);
+
+        BagManagerWriter bw = null;
+
+        if (eogroup != null) {
+            bw = new BagManagerWriter(bagdir, eogroup.outbagdir, algorithm, eogroup.replace);
+        } else {
+            bw = new BagManagerWriter(bagdir, algorithm);
+        }
+
         return bw.write();
     }
 }
